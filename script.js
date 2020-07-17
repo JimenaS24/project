1f19bcd240ef7d9262a3ae72a5e69bc2a48555a3
JimenaS24 <68298384+JimenaS24@users.noreply.github.com>
2020-07-17 10:13:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function setup(){
-  createCanvas(600,600);
-  frameRate(75);
-  background(24, 214, 144);
-  }
- 
- 
- function draw(){
-  if (GameOn == true){
-  //start of GameOn
-  background(24, 214, 144);
-  noStroke();
-  fill(0,0,0)
- 
-  if(lives ==3){
-  div1{
-  width: 20PX
-  height: 20PX
-  fill:red;}
-  
+// select canvas
+const cvs = document.getElementById("pong");
+
