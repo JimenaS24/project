3b849aedf13eb4f0669ca7679818d5fd7434527b
JimenaS24 <68298384+JimenaS24@users.noreply.github.com>
2020-07-17 09:23:02 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function setup(){
+  createCanvas(600,600);
+  frameRate(75);
+  background(24, 214, 144);
+  }
+ 
+ 
+ function draw(){
+  if (GameOn == true){
+  //start of GameOn
+  background(24, 214, 144);
+  noStroke();
+  fill(0,0,0)
+ 
+  if(lives ==3){
+  div1{
+  width: 20PX
+  height: 20PX
+  fill:red;}
+  
