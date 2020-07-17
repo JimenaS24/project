949c8107f86fc341606fb6aba79960e4681f876e
JimenaS24 <68298384+JimenaS24@users.noreply.github.com>
2020-07-17 10:16:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // select canvas
 const cvs = document.getElementById("pong");
+const ctx = cvs.getContext("2d");
+
+//draw rect function
+
+function drawRect(x,y,w,h,color){
+  ctx.fillstyle = color;
+  ctx.fillRect(x,y,w,h);
+}
+
+drawRect(0,0, cvs.width, cvs.height, "BLACK");
+
+//draw circle
+
 
